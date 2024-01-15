9967957f502cbced4dd9e1f2b36485e33d216f20
Emir Hamidovic <emir.hamidovic98@gmail.com>
2024-01-15 21:46:38 +0100

    Improvement to the home page frontend

--- frontend/src/components/HomePage.tsx
+++ frontend/src/components/HomePage.tsx
@@ -3,17 +3,35 @@ import Polls from "./Polls";
 import { Box } from "@mui/material";
 import { Button } from "react-bootstrap";
 import useAuth from "../hooks/use-auth";
+import '../css/HomePage.css';
 
 const HomePage = () => {
+
+	const createPollStyles = {
+    background: 'lightblue', // Set your desired background color
+    width: '20%', // Set your desired width
+    padding: '10px', // Set any additional styling
+    borderRadius: '8px', // Set border-radius for rounded corners // Center the box horizontally
+    textAlign: 'center', // Center text within the box
+	height: '15%',
+	color: 'white',
+	fontWeight: 'bold',
+	fontSize: '16px',
+	marginLeft: '2rem',
+	marginTop: '4.5rem',
+  };
 	const navigate = useNavigate();
 	const {auth} = useAuth();
 	return (
-		<div className="App">
+		<div className="centered-polls">
 			<Polls />
-			{auth.user ? <Box textAlign="center" mt={1}>
+			{auth.user ? <Box sx={createPollStyles} mt={1}>
+			<div className="createPollText">
+				<p>Interested in making a new poll? Create a new poll and contribute to the community!</p>
 				<Button variant="contained" color="primary" onClick={() => navigate("/create-poll")}>
 				Create New Poll
 				</Button>
+				</div>
 			</Box> : ''}
 		</div>
 	);
--- frontend/src/components/Polls.tsx
+++ frontend/src/components/Polls.tsx
@@ -2,48 +2,68 @@ import { useCallback, useEffect, useState } from 'react';
 import { useNavigate } from 'react-router-dom';
 import useAuth from '../hooks/use-auth';
 import axiosInstance from '../utils/axios-instance';
+import '../css/Polls.css';
 
 const Polls = () => {
-	const navigate = useNavigate();
-	const {auth} = useAuth();
-	const [polls, setPolls] = useState([]);
-	const fetchData = useCallback(async () => {
-		try {
-			const response = await axiosInstance.get('/polls/get');
-			setPolls(response.data);
-		} catch (error) {
-			console.error('Error fetching data:', error);
-		}
-	}, []);
+    const navigate = useNavigate();
+    const { auth } = useAuth();
+    const [polls, setPolls] = useState([]);
+    const fetchData = useCallback(async () => {
+        try {
+            const response = await axiosInstance.get('/polls/get');
+            setPolls(response.data);
+        } catch (error) {
+            console.error('Error fetching data:', error);
+        }
+    }, []);
 
-	useEffect(() => {
-		// Fetch data from Go server
-		fetchData();
-	}, [fetchData]);
+    useEffect(() => {
+        // Fetch data from Go server
+        fetchData();
+    }, [fetchData]);
 
-	return (
+    return (
 		<div className="polls">
-				{polls && polls.map(poll => (
-					<div key={poll['id']} className="poll">
-						<div className="poll-image">
-							<img src={`${poll['image']}`} alt={poll['name']} />
-						</div>
-						<div className="poll-details">
-							<h2>{poll['name']}</h2>
-							<p>{poll['description']}</p>
-							<p>Season: {poll['season']}</p>
-						</div>
-						<div className="poll-actions">
-							{auth.user ?
-							<button className="vote-button" onClick={() => navigate(`/poll/${poll['id']}`)}>Vote</button> :
-							''
-							}
-							<button className="results-button" onClick={() => navigate(`/results/${poll['id']}`)}>Check Results</button>
-						</div>
-					</div>
-				))}
-		</div>
-	);
+  <h1>Editors' Picks</h1>
+  <div className="polls-grid">
+    {polls &&
+      polls.map((poll) => (
+        <div key={poll['id']} className="poll">
+          <div className="poll-details">
+            <h2>{poll['name']}</h2>
+            <div className="image-description-row">
+              <div className="poll-image">
+                <img src={`${poll['image']}`} alt={poll['name']} />
+              </div>
+              <div className="poll-description">
+                <p>{poll['description']}</p>
+                <p>Season: {poll['season']}</p>
+              </div>
+            </div>
+			<div className="poll-actions">
+            {auth.user && (
+                <button
+                  className="vote-button"
+                  onClick={() => navigate(`/poll/${poll['id']}`)}
+                >
+                  Vote
+                </button>
+			)}
+                <button
+                  className="results-button"
+                  onClick={() => navigate(`/results/${poll['id']}`)}
+                >
+                  Check Results
+                </button>
+              </div>
+          </div>
+        </div>
+      ))}
+  </div>
+</div>
+
+  
+    );
 };
 
 export default Polls;
